d46af8d6041fc8d4b14b7551aea89c007c8df0cc
Prabal Gupta <152983979+1prabal@users.noreply.github.com>
2026-08-02 05:14:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (70.97%) - LeetSync

---
+++ 27-remove-element/remove-element.c
@@ -0,0 +1,10 @@
+int removeElement(int* nums, int n, int val) {
+    int i = 0;
+    for (int j = 0; j < n; j++) {
+        if (nums[j] != val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+}
